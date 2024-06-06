e365d5340baf01674592498e219146c9f154e732
mosheWarshaw <mosheawarshaw@gmail.com>
2024-06-06 15:05:46 -0400

    Improed doc-comments' formatting.

--- In Memory Db/src/Query/Funcs/Select.cs
+++ In Memory Db/src/Query/Funcs/Select.cs
@@ -11,24 +11,28 @@ namespace InMemoryDb
     public partial class Funcs
     {
         /// <summary>
-        /// cols:
-        /// Can't be of length 0.
-        /// Also, declare them in the order you want them to be filled,
-        /// so that if col% wants to access col# in its lambda, then col#
-        /// should be in the arrray before col#.
-        /// 
-        /// where:
-        /// This only gets access to the fields of the row just created.
-        /// If you want to do a correlated subquery then do 2 chained Selects, and
-        /// do that outer select after the inner select (remember, a tablename for a Select
-        /// can be the anme you gave to the result of a previous query).
-        /// 
-        /// 
-        /// nameOfResultTable:
-        /// Cannot be set to null, else it will be ignored.
-        /// 
-        /// amountToTake:
-        /// If set, the function will take the first n amount of rows that pass the where function, where n = amountToTake.
+        /// <para>
+        ///     <c>cols</c>:
+        ///     Can't be of length 0.
+        ///     Also, declare them in the order you want them to be filled,
+        ///     so that if col% wants to access col# in its lambda, then col#
+        ///     should be in the arrray before col#.
+        /// </para>
+        /// <para>
+        ///     <c>where</c>:
+        ///     This only gets access to the fields of the row just created.
+        ///     If you want to do a correlated subquery then do 2 chained Selects, and
+        ///     do that outer select after the inner select (remember, a tablename for a Select
+        ///     can be the anme you gave to the result of a previous query).
+        /// </para>
+        /// <para>
+        ///     <c>nameOfResultTable</c>:
+        ///     Cannot be set to null, else it will be ignored.
+        /// </para>
+        /// <para>
+        ///     <c>amountToTake</c>:
+        ///     If set, the function will take the first n amount of rows that pass the where function, where n = amountToTake.
+        /// </para>
         /// </summary>
         public Funcs Select(string tableName, ICol[] cols, Func<SameRowAccessor, bool> where = null, string nameOfResultTable = null, int? amountToTake = null)
         {
